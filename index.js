7aa83b9cfe02a34ef36383b55f598b900ed9da53
Kaisha Smith <smith.kaisha04@gmail.com>
2020-05-19 20:40:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase()) {
     return ("I can't hear you!")
   }
-  if (string.toUpperCase()) {
+  else if (string.toUpperCase()) {
     return ("YES INDEED!")
   }
- if (string === "I love you, Grandma."){
+ else if (string === "I love you, Grandma."){
     return ("I love you, too.")
  }
 }
